4290d033a5c0b4194951d6287cb885725b41fc50
rduvalwa5 <rduvalwa5@hotmail.com>
2015-03-27 00:19:28 -0700

    build out string test

--- PythonExamples/src/test_PyString.py
+++ PythonExamples/src/test_PyString.py
@@ -32,8 +32,31 @@ class Test_PyrString(unittest.TestCase):
         
     def testMakeCaps(self):
         s1 = PyString.py_string()
-        inpt = "one two three four five."
-        expected = "One two three four five."
+        inpt = "one two three four five.  four five six."
+        expected = "One two three four five.  four five six."
         self.assertEqual(expected,s1.makeCaps(inpt))
+        
+    def testSplitString(self):
+        s1 = PyString.py_string()
+        inpt = "one two three four five.  Six seven eight?"
+        expected = ['one', 'two', 'three', 'four', 'five.', '', 'Six', 'seven', 'eight?']
+        self.assertEqual(expected, s1.splitString(inpt, " "))
+        
+    def testStrngJoin(self):
+        s1 = PyString.py_string()
+        separator = "+"
+        seq = ("one","two","three","four")
+        expected = "one+two+three+four"
+        self.assertEqual(expected,s1.stringJoin(separator,seq))
+        
+    def testStringReplace(self):
+        s1 = PyString.py_string()
+        original = "two"
+        replacement = "twelve"
+        maxreplace = 2
+        text = "one two three four five two.  Six seven eight two?"
+        expected = "one twelve three four five twelve.  Six seven eight two?"
+        self.assertEqual(expected,s1.stringReplace(text, original, replacement, maxreplace))
+        
 if __name__ == "__main__":
     unittest.main()
