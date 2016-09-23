8c552cecf5d1f0e1040e2a30ded8732194077a29
rduvalwa5 <rduvalwa5@hotmail.com>
2016-09-23 00:14:14 -0700

    add new datetime examples

---
+++ Py_DateTime/src/date_time_formating.py
@@ -0,0 +1,33 @@
+'''
+Created on Sep 22, 2016
+These classes explain date time formating
+https://docs.python.org/3/search.html?q=date+time&check_keywords=yes&area=default
+https://docs.python.org/3/library/datetime.html
+
+'''
+import datetime
+
+
+class ExamplesDateTimeFormat(object):
+    '''
+    classdocs
+    '''
+
+
+#    def __init__(self, params):
+#        '''
+#        Constructor
+#        '''
+        
+    def display_min_max(self):
+        min = datetime.MINYEAR
+        max = datetime.MAXYEAR
+        values = {'min':min,'max':max}
+        return values
+        
+        
+if __name__ == '__main__':
+    myClass = ExamplesDateTimeFormat()
+    min_max = myClass.display_min_max()
+    print("minimum datetime is: ", min_max.get('min'))
+    print("maximum datetime is: ", min_max.get('max'))
