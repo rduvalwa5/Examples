71255776a732819095abbb7bc8b93306b028be54
rduvalwa5 <rduvalwa5@hotmail.com>
2016-10-01 12:51:52 -0700

    enhance changes to tzinfo

--- Py_DateTime/src/py_tzinfo_examples.py
+++ Py_DateTime/src/py_tzinfo_examples.py
@@ -35,14 +35,12 @@ class localtime_tzinfo(object):
         self.hour = h
         self.min = m
         self.sec =s
-        self.this_tzinfo = timezone(self.region)
-        
-    def set_utc(self):
         self.utc = pytz.utc
-        return  self.utc
-    
+        self.this_tzinfo = timezone(self.region)
+        self.utc_dt = datetime(self.year,self.month,self.day,self.hour,self.min,self.sec, tzinfo=self.utc)
+        
     def get_utc(self):
-        return self.set_utc()
+        return self.utc_dt
         
     def get_utc_zone(self):
         return  self.utc.zone
@@ -67,6 +65,37 @@ class localtime_tzinfo(object):
     
     def display_local_timezone2(self):
         return datetime(self.year,self.month,self.day,self.hour,self.min,self.sec, tzinfo = self.this_tzinfo ).strftime(self.fmt)
+
+    '''
+    
+    '''
+    def convert_from_utc(self):
+        '''
+        The preferred way of dealing with times is to always work in UTC, converting to localtime only when generating output 
+        to be read by humans.
+        >>> utc_dt = datetime(2002, 10, 27, 6, 0, 0, tzinfo=utc)
+        >>> loc_dt = utc_dt.astimezone(eastern)
+        >>> loc_dt.strftime(fmt)
+        '2002-10-27 01:00:00 EST-0500'
+        '''
+        loc_dt = self.utc_dt.astimezone(self.this_tzinfo)
+        return loc_dt.strftime(self.fmt)
+        
+        
+    def date_arithmetic(self):
+        '''
+        This library also allows you to do date arithmetic using local times, although it is more complicated than 
+        working in UTC as you need to use the normalize() method to handle daylight saving time and other timezone transitions. 
+        In this example, loc_dt is set to the instant when daylight saving time ends in the US/Eastern timezone.
+        >>> before = loc_dt - timedelta(minutes=10)
+        >>> before.strftime(fmt)
+        '2002-10-27 00:50:00 EST-0500'
+        >>> eastern.normalize(before).strftime(fmt)
+        '2002-10-27 01:50:00 EDT-0400'
+        >>> after = eastern.normalize(before + timedelta(minutes=20))
+        >>> after.strftime(fmt)
+        '2002-10-27 01:10:00 EST-0500'
+        '''
     
 if __name__ == '__main__':
     eastLocal = localtime_tzinfo(2016,12,31,23,59,59,'US','Eastern')
@@ -92,6 +121,7 @@ if __name__ == '__main__':
     print(hawaiiLocal.get_regionTimeZone_tzinfo())
     
     print(moscowLocal.display_local_timezone())
+    print('convert_from uts ',moscowLocal.convert_from_utc())
     print(eastLocal.display_local_timezone())
     print(centralLocal.display_local_timezone())
     print(westLocal.display_local_timezone())
