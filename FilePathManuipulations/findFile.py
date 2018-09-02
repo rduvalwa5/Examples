61c80ce9b0b3476bdfffb262d56c2593e13c1025
rduvalwa5 <rduvalwa5@hotmail.com>
2018-09-01 17:05:28 -0700

    clean up findFile

--- FilePathManuipulations/findFile.py
+++ FilePathManuipulations/findFile.py
@@ -1,9 +1,11 @@
+'''
+findFile locates a file within a path and returns a list of locations and the number of files examined to find it.
+'''
 #!/usr/local/bin/python
 
 import glob
 import os
 import sys
-
 
 def find_file(fil,path):
         ListFiles = []
@@ -12,19 +14,27 @@ def find_file(fil,path):
         for root, dirs, files in os.walk(path, topdown=True):
             for f in files:
                 fileCount = fileCount + 1
-                print(fileCount, "  " , f) # , "  ", root)            
+#                print(fileCount, "  " , f) # , "  ", root)            
                 if os.path.isfile(os.path.join(root, f)):
                     if f == fil:
                         ListFiles.append(root + "/" + f) 
-                        ListFiles.append(fileCount)    
- 
+                        ListFiles.append(fileCount)     
         return (ListFiles, fileCount)
  
 if __name__ == "__main__":
-
+    import platform
 #    print(find_file(sys.argv[1],sys.argv[2]))
-    print(find_file("findFile.py","/Users/rduvalwa2/python_workspace2017/Examples"))
+
+    if platform.uname().node == 'OSXAir.home.home':
+        print(find_file("findFile.py","/Users/rduvalwa2/eOxigen-workspace/Examples"))
+    elif platform.uname().node == 'C1246895-osx.home':
+        print(find_file("findFile.py","/Users/rduvalwa2/python_workspace2017/Examples"))
+    elif platform.uname().node ==  'C1246895-XPS':    
+        print(find_file("findFile.py","/Users/rduvalwa2/python_workspace2017/Examples"))
+    else: 
+        print(find_file("findFile.py","/Users/rduvalwa2/eOxigen-workspace/Examples"))
     
     '''
     (['/Users/rduvalwa2/python_workspace2017/Examples/FilePathManuipulations/findFile.py', 332], 441)
+
     '''
