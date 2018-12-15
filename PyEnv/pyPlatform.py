0f4db53267d625100e3b996efc11344a6a0f1b5f
rduvalwa5@hotmail.com <rduvalwa5@hotmail.com>
2018-12-15 12:25:35 -0800

    add platform examples

--- PyEnv/pyPlatform.py
+++ PyEnv/pyPlatform.py
@@ -7,9 +7,10 @@ import platform
 
 print("system ",platform.system())
 print("machine ",platform.machine())
+print("processor ",platform.processor())
 print("uname node ", platform.uname().node)
-print("process ",platform.processor())
 print("python version ", platform.python_version())
 print("python implementation ", platform.python_implementation())
 print("python release ", platform.release())
+print("release alias ", platform.system_alias(platform.release(),platform.release(),platform.version()))
 print("Mac version ", platform.mac_ver())
