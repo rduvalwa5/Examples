a76bb1039bf597dbae13d4a75824f69be7ec9fd0
rduvalwa5 <rduvalwa5@hotmail.com>
2020-01-20 16:24:02 -0800

    update file transfer

--- FilePathManuipulations/MyFileTransfer.py
+++ FilePathManuipulations/MyFileTransfer.py
@@ -2,12 +2,13 @@
 Created on Jan 16, 2020
 https://stackoverflow.com/questions/12909334/how-to-transfer-files-from-client-to-server-computer-by-using-python-script
 @author: rduvalwa2
+
 '''
 import paramiko
 
-source = r'/Users/rduvalwa2/TestDir/FileTransfer.txt'
-dest = r'/Users/rduvalwa2/TestDir/2FileTransfer.txt'
-hostname = '10.0.0.57'
+source = r'/Users/rduvalwa2/TestDir/Music_2020-01-20.sql'
+dest = r'/Users/rduvalwa2/TestDir/Music_2020-01-20.sql'
+hostname = 'RandalluvalsMBP'  # RandalluvalsMBP  C1246895-OSX
 port = 22 # default port for SSH
 username = 'rduvalwa2'
 password = 'blu4jazz'
