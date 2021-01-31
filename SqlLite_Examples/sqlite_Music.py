af80955a91d7c7777fa4b36dd06714759986cc49
rduvalwa2 <rduvalwa2@osxair.hsd1.wa.comcast.net>
2021-01-30 17:04:26 -0800

    refactor code

--- SqlLite_Examples/sqlite_Music.py
+++ SqlLite_Examples/sqlite_Music.py
@@ -3,43 +3,136 @@ Created on Jan 27, 2021
 https://docs.python.org/3/library/sqlite3.html
 https://www.thegeekstuff.com/2012/09/sqlite-command-examples/
 https://sqlite.org/datatype3.html#expraff
+https://www.kite.com/python/answers/how-to-list-tables-using-sqlite3-in-python
+
+https://www.programiz.com/python-programming/list
+https://www.tomordonez.com/get-schema-sqlite-python/
+
+https://www.sqlitetutorial.net/sqlite-python/sqlite-python-select/
+
 @author: rduvalwa2
+
+MySQL -------
+INSERT INTO `type` (`type`, `type_idx`)
+VALUES
+    ('TestType',1),
+    ('Download',2),
+    ('Vinyl',3),
+    ('CD',4),
+    ('Tape',5),
+    ('TestType',6);
+
+-- Values stored as TEXT, INTEGER, INTEGER, REAL, TEXT.
+
+sqlite> INSERT INTO type VALUES(1,'TestTYpe');
+
+sqlite> select * from type;
+1|TestTYpe
+
 '''
+
+
 import sqlite3
+from sqlite3 import Error
 
-con = sqlite3.connect(":Music.db:")
-con.isolation_level = None
-cur = con.cursor()
 
-buffer = ""
 
-conn = sqlite3.connect("Music.db")
-c = conn.cursor()
-#c.execute('''create table stocks
-#(date text, trans text, symbol text,
-# qty real, price real)''')
-#c.execute("""insert into stocks
-#          values ('2006-01-05','BUY','RHAT',100,35.14)""")
-c.execute("tables")
-print (c.fetchall())
-conn.commit()
+    
 
-conn.row_factory = sqlite3.Row
-c = conn.cursor()
-c.execute('select * from type')
-# <sqlite3.Cursor object at 0x7f4e7dd8fa80>
-r = c.fetchone()
-type(r)
-# <type 'sqlite3.Row'>
-r
-# (u'2006-01-05', u'BUY', u'RHAT', 100.0, 35.14)
-len(r)
-# 5
-r[2]
-# u'RHAT'
-r.keys()
-# ['date', 'trans', 'symbol', 'qty', 'price']
-r['qty']
-# 100.0
-for member in r:
-    print( member)
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by the db_file
+    :param db_file: database file
+    :return: Connection object or None   
+    """
+    
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print("connection complete")
+
+              
+    except Error as e:
+        print(e)
+
+    return conn
+
+def get_data_tables(con):
+    cur = con.cursor()
+    cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    myTables = cur.fetchall()
+    print(myTables)
+    allTables = []
+    for tab in myTables:
+        allTables.append(tab[0])
+    print(allTables)
+    return allTables
+    
+
+
+def describe_table(conn,table):
+    """
+    .schema table_name
+    cur.execute("PRAGMA table_info('Courses')").fetchall()
+    """
+    cur = conn.cursor()
+    print(table)
+    print(cur.execute("PRAGMA table_info(" + table + ")").fetchall())
+#    rows = cur.execute("PRAGMA table_info('artist_albums')").fetchall()
+#    for row in rows:
+#        print(row)
+        
+
+def insert_artist_albums():
+    pass
+
+def select_all_type(conn):
+    """
+    Query all rows in the tasks table
+    :param conn: the Connection object
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM type;")
+
+    rows = cur.fetchall()
+    print("rows are ",rows)
+    for row in rows:
+        print(row)
+
+
+def select_type_by_type(conn, t):
+    """
+    Query tasks by priority
+    :param conn: the Connection object
+    :param priority:
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM type WHERE type like ?",(t,))
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+        
+
+
+
+def main():
+    database = r"/Users/rduvalwa2/Music.db"
+
+    # create a database connection
+    conn = create_connection(database)
+    with conn:
+#        select_all_type(conn)
+#        select_type_by_type(conn,'Vinyl')
+        allTables = get_data_tables(conn)
+        for tab in allTables:
+            print(tab)
+            describe_table(conn,tab)
+        
+
+
+if __name__ == '__main__':
+    main()
