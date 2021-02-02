a2d5d31807be21c0fc7dfb45aa7d08d0c031ed36
rduvalwa2 <rduvalwa2@osxair.hsd1.wa.comcast.net>
2021-02-01 16:14:25 -0800

    sqllite load code

--- SqlLite_Examples/sqlite_Music.py
+++ SqlLite_Examples/sqlite_Music.py
@@ -71,59 +71,7 @@ def describe_table(conn,table):
     cur = conn.cursor()
     print(table)
     print(cur.execute("PRAGMA table_info(" + table + ")").fetchall())
-#    rows = cur.execute("PRAGMA table_info('artist_albums')").fetchall()
-#    for row in rows:
-#        print(row)
-        
-
-def insert_album_covers(conn):
-#    cur = conn.cursor()
-    filename = '/Users/rduvalwa2/git/Examples/SqlLite_Examples/Sqlite_Notes/album_covers.csv'
-    print(filename)
-    with open(filename, newline='') as f:
-        reader = csv.reader(f)
-    try:
-        rowCount = 0
-        for row in reader:
-            if rowCount > 0:
-                print(row[0], row[1], row[2], row[3])
-            rowCount = rowCount + 1
-            print(rowCount)
-    except csv.Error as e:
-        sys.exit('file {}, line {}: {}'.format(filename, reader.line_num, e))
-    
-    
-    
-    
-    
-    """
-    filename = '/Users/rduvalwa2/git/Examples/SqlLite_Examples/Sqlite_Notes/album_covers.csv'
-    print(filename)
-    #over_idx,"album_cover","album","description"
-    with open(filename, newline='') as f:
-        reader = csv.reader(f)
-        try:
-            rowCount = 0
-            for row in reader:
-                if rowCount > 0:
-                    print(row[0], row[1], row[2], row[3])
-#                statement = "INSERT INTO album_covers VALUES(cover_idx,album_cover,album,description),(cover_idx,album_cover,album,description);)
-#                    statement = "INSERT INTO album_covers VALUES(" + row[0]+ " \, " + row[1]+ "\, " + row[2] + "\, " + row[3] + ";"
-#                    print(statement)
-            rowCount = rowCount + 1
-            print(rowCount)
-
-        except csv.Error as e:
-            sys.exit('file {}, line {}: {}'.format(filename, reader.line_num, e))
-    
-#    cur.execute("SELECT * FROM type WHERE type like ?",(t,))
-#    cur.execute("INSERT INTO album_covers VALUES('cover_idx','album_cover','album','description');"
-#    sqlite> INSERT INTO type VALUES(1,'TestTYpe');
-
-#    statement = "INSERT INTO album_covers VALUES(cover_idx,album_cover,album,description),(cover_idx,album_cover,album,description);)
-#    cur.execute("INSERT INTO album_covers VALUES('cover_idx','album_cover','album','description');",(cover_idx,album_cover,album,description))
-    
-"""
+  
 def select_all_type(conn):
     """
     Query all rows in the tasks table
@@ -153,9 +101,6 @@ def select_type_by_type(conn, t):
 
     for row in rows:
         print(row)
-        
-
-
 
 def main():
     database = r"/Users/rduvalwa2/Music.db"
@@ -164,12 +109,11 @@ def main():
     conn = create_connection(database)
     with conn:
         select_all_type(conn)
-#        select_type_by_type(conn,'Vinyl')
-#        allTables = get_data_tables(conn)
-#        for tab in allTables:
-#            print(tab)
-#            describe_table(conn,tab)
-        insert_album_covers(conn)
+        select_type_by_type(conn,'Vinyl')
+        allTables = get_data_tables(conn)
+        for tab in allTables:
+            print(tab)
+            describe_table(conn,tab)
 
 
 if __name__ == '__main__':
